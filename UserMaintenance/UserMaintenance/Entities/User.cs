5792bb71d232a9b6446edce023fb8aaaa6072e60
Filep Illés <filep.illes@hotmail.com>
2020-09-22 16:35:02 +0200

    Átírva FullName-ra

--- UserMaintenance/UserMaintenance/Entities/User.cs
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -9,16 +9,10 @@ namespace UserMaintenance.Entities
     public class User
     {
         public Guid ID { get; set; } = Guid.NewGuid();
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+
         public string FullName
         {
-            get
-            {
-                return $"{LastName} {FirstName}";
-
-            }
+            get;set;
         }
-
     }
 }
