373443d638b6c10625327e0151f9050f23520080
Filep Illés <filep.illes@hotmail.com>
2020-10-09 18:13:46 +0200

    Adatok betöltése

--- Portfoliomanagement/Portfoliomanagement/Form1.cs
+++ Portfoliomanagement/Portfoliomanagement/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,18 @@ namespace Portfoliomanagement
 {
     public partial class Form1 : Form
     {
+        List<Tick> Ticks;
+        PortfolioEntities context = new PortfolioEntities();
         public Form1()
         {
             InitializeComponent();
+            loadData();
+        }
+        private void loadData()
+        {
+            context.Ticks.Load();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
