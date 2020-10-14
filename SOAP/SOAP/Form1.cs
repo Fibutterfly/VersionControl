788a957ec4d0d28db46ef5bb66a92506f52baad7
Filep Illés <filep.illes@hotmail.com>
2020-10-14 17:46:35 +0200

    Webszolgáltatás hívása

--- SOAP/SOAP/Form1.cs
+++ SOAP/SOAP/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using SOAP.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,29 @@ namespace SOAP
         public Form1()
         {
             InitializeComponent();
+            LoadWebService();
         }
+        private void LoadWebService()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+
+            };
+            GetExchangeRatesResponseBody response = mnbService.GetExchangeRates(request);
+            string result = response.GetExchangeRatesResult;
+            writeToFile("Eur_20_01_01__20_06_30.xml",result);
+        }
+        private void writeToFile(string name, string data)
+        {
+            using (StreamWriter sw = new StreamWriter(name))
+            {
+                sw.WriteLine(data);
+            }
+        }
+
     }
 }
