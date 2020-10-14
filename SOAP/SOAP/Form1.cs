58002242abac128d22d529170267e2831636031f
Filep Illés <filep.illes@hotmail.com>
2020-10-14 18:11:07 +0200

    XML feldolgozása

--- SOAP/SOAP/Form1.cs
+++ SOAP/SOAP/Form1.cs
@@ -1,4 +1,5 @@
-﻿using SOAP.MnbServiceReference;
+﻿using SOAP.Entities;
+using SOAP.MnbServiceReference;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,17 +10,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace SOAP
 {
     public partial class Form1 : Form
     {
         BindingList<Entities.RateData> Rates = new BindingList<Entities.RateData>();
+        string result;
+
         public Form1()
         {
             InitializeComponent();
             LoadWebService();
             dataGridView1.DataSource = Rates;
+            ProcessXML();
+
+        }
+        private void ProcessXML()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var childElement = (XmlElement)element.ChildNodes[0];
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                var rate = new RateData()
+                {
+                    Date = DateTime.Parse(element.GetAttribute("date")),
+                    Currenty = childElement.GetAttribute("curr"),
+                    Value = (unit != 0)?(value/unit):(default)
+                };
+                Rates.Add(rate);
+            }
         }
         private void LoadWebService()
         {
@@ -31,8 +55,8 @@ namespace SOAP
                 endDate = "2020-06-30"
 
             };
-            GetExchangeRatesResponseBody response = mnbService.GetExchangeRates(request);
-            string result = response.GetExchangeRatesResult;
+            var response = mnbService.GetExchangeRates(request);
+            result = response.GetExchangeRatesResult;
             writeToFile("Eur_20_01_01__20_06_30.xml",result);
         }
         private void writeToFile(string name, string data)
