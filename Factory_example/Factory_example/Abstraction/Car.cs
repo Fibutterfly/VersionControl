1921a4b19fcbcbe3c1aee3d48b56850f8464181c
Filep Illés <filep.illes@hotmail.com>
2020-11-12 16:45:47 +0100

    Car osztály

---
+++ Factory_example/Factory_example/Abstraction/Car.cs
@@ -0,0 +1,19 @@
+﻿using Factory_example.Entites;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_example.Abstraction
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics input)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            input.DrawImage(imageFile, new Rectangle(0,0,Width,Height));
+        }
+    }
+}
