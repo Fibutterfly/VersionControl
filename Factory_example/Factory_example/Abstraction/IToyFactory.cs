78e01899d15b7ce0c096290e3993b3cf74a93dab
Filep Illés <filep.illes@hotmail.com>
2020-11-12 16:51:08 +0100

    Interface

---
+++ Factory_example/Factory_example/Abstraction/IToyFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Factory_example.Entites;
+
+namespace Factory_example.Abstraction
+{
+    interface IToyFactory
+
+    { 
+        Toy CreateNew();
+    }
+}
