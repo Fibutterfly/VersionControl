af668853e7aaabd256dff779383957a602499eeb
Illés Attila Filep <filep.illes@hotmail.com>
2020-11-20 17:48:38 +0100

    Jelszó validálás teszt

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -10,6 +10,7 @@ namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
+        AccountController accountController = new AccountController();
         [
             Test,
             TestCase("abcd1234", false),
@@ -18,15 +19,25 @@ namespace UnitTestExample.Test
             TestCase("irf@uni-corvinus.hu", true)
         ]
         public void TestValidateEmail(string email, bool expectedResult)
-        {
-            // Arrange
-            var accountController = new AccountController();
-
+        {           
             // Act
             var actualResult = accountController.ValidateEmail(email);
 
             // Assert
             Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+        [
+            Test,
+            TestCase("nInCsSzAm",false),
+            TestCase("NINCSKISBETU123",false),
+            TestCase("nincsnagybetu123",false),
+            TestCase("rOvId12",false),
+            TestCase("sZuPeRjeLsZo123",true)
+        ]
+        public void TestValidatePassword(string dummypass, bool exceptedResult)
+        {
+            var actualResult = accountController.ValidatePassword(dummypass);
+            Assert.That(actualResult, Is.EqualTo(exceptedResult));
         }
     }
 }
